2c75b9577c26bc026f01bae44da70c13f93f5064
Maz <markuspietila95@gmail.com>
2020-03-04 19:49:06 +0200

    Updates

---
+++ precache-manifest.bbb8bf0af9cf7b613f218c2d8ad479a5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cf46bbcfda8d42da306786075ac724",
+    "url": "/LaserGridWeb/index.html"
+  },
+  {
+    "revision": "2ec91460a5db42680004",
+    "url": "/LaserGridWeb/static/css/main.6ae11a09.chunk.css"
+  },
+  {
+    "revision": "a1d10441df2d2347b995",
+    "url": "/LaserGridWeb/static/js/2.2104c36a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/LaserGridWeb/static/js/2.2104c36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec91460a5db42680004",
+    "url": "/LaserGridWeb/static/js/main.03a9f029.chunk.js"
+  },
+  {
+    "revision": "c0dd00c925e117cc383a",
+    "url": "/LaserGridWeb/static/js/runtime-main.a6143e59.js"
+  },
+  {
+    "revision": "f5c5ea4fe7d9e0c854ed1ab02137fe3c",
+    "url": "/LaserGridWeb/static/media/LG_FLAME.f5c5ea4f.jpg"
+  },
+  {
+    "revision": "0e1123dc80d97bfe1a1b461b61d2972c",
+    "url": "/LaserGridWeb/static/media/LG_HP.0e1123dc.jpg"
+  },
+  {
+    "revision": "c8edfebbcf633fea054ea8907b28ef03",
+    "url": "/LaserGridWeb/static/media/LG_LASER.c8edfebb.jpg"
+  },
+  {
+    "revision": "e5cf84a1386098bef4f9ff85e1888c1c",
+    "url": "/LaserGridWeb/static/media/LG_MAP1.e5cf84a1.jpg"
+  },
+  {
+    "revision": "ceb0d1d9ed383a7bab1eec112c3137a3",
+    "url": "/LaserGridWeb/static/media/LG_MAP2.ceb0d1d9.jpg"
+  },
+  {
+    "revision": "ce744f6957300fd4094d6c639f7e9914",
+    "url": "/LaserGridWeb/static/media/LG_MINE.ce744f69.jpg"
+  },
+  {
+    "revision": "6ad76e92d78616990833724778ef8901",
+    "url": "/LaserGridWeb/static/media/LG_SHIELD.6ad76e92.jpg"
+  },
+  {
+    "revision": "4943f0a0df9e9239b58e5dc6cf72cfda",
+    "url": "/LaserGridWeb/static/media/LG_TORPEDO.4943f0a0.jpg"
+  },
+  {
+    "revision": "803f74782e79d52eb1bc433509fd7498",
+    "url": "/LaserGridWeb/static/media/build-ICON.803f7478.png"
+  },
+  {
+    "revision": "aa84e05893975005bc56f8a03100942b",
+    "url": "/LaserGridWeb/static/media/credits_background.aa84e058.png"
+  },
+  {
+    "revision": "a24fa2e9069cfbc5f82d4da65e69f067",
+    "url": "/LaserGridWeb/static/media/logoready.a24fa2e9.png"
+  },
+  {
+    "revision": "53081755cf23954f56a1bbdc864cabc4",
+    "url": "/LaserGridWeb/static/media/starry sky.53081755.png"
+  }
+]);
